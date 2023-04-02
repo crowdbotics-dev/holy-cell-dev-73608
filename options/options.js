7479b55b8fc881394622df5708f5a0185b7b1062
Crowdbotics <engineering@crowdbotics.com>
2023-04-02 00:08:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-cell-dev-73608",
+  url: "https://holy-cell-dev-73608.botics.co",
+  api: "https://holy-cell-dev-73608.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
